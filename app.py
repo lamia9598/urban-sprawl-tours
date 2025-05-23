a0383c09a5d9f7a6d912cb5a5c4e351e6668bb38
lamia9598 <lamiainnab5@gmail.com>
2025-05-23 15:18:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,12 @@ m = leafmap.Map(center=[47.39, 0.688], zoom=11, esri=True)
 
 # Define raster layers (projected to EPSG:4326)
 raster_layers = {
-    "Urban cover 2015 (Blue)": ("https://huggingface.co/datasets/lamia9598/app/blob/main/urban_cover_2015_cog.tif", "Blues"),
-    "Urban cover 2040 BAU (Red)": ("https://huggingface.co/datasets/lamia9598/app/blob/main/urban_cover_2040_bau_cog.tif", "Reds"),
-    "Urban cover 2040 Compact (Green)": ("https://huggingface.co/datasets/lamia9598/app/blob/main/urban_cover_2040_compact_cog.tif", "Greens"),
-    "Soil Vulnerability (Magma)": ("https://huggingface.co/datasets/lamia9598/app/blob/main/soil_vulnerability_cog.tif", "magma")
+    "Urban cover 2015 (Blue)": ("https://huggingface.co/datasets/lamia9598/app/resolve/main/urban_cover_2015_cog.tif", "Blues"),
+    "Urban cover 2040 BAU (Red)": ("https://huggingface.co/datasets/lamia9598/app/resolve/main/urban_cover_2040_bau_cog.tif", "Reds"),
+    "Urban cover 2040 Compact (Green)": ("https://huggingface.co/datasets/lamia9598/app/resolve/main/urban_cover_2040_compact_cog.tif", "Greens"),
+    "Soil Vulnerability (Magma)": ("https://huggingface.co/datasets/lamia9598/app/resolve/main/soil_vulnerability_cog.tif", "magma")
 }
+
 
 # Display layers with high-quality settings
 for label, (file_path, cmap) in raster_layers.items():
@@ -46,3 +47,4 @@ else:
 # Enable layer control and display map
 m.add_layer_control()
 m.to_streamlit(width=1200, height=700)
+
